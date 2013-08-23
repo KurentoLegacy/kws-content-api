d1382aa6fdb9daca3938c31f1a869d70d2d7bd50
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2013-08-23 14:53:48 +0200

    Initial version of demo app for CampusParty Europe 2013 at London
    Change-Id: Iaa9599a5924ac424dddde2f53ce1baadcfed5536

---
+++ src/main/resources/demo/js/index.js
@@ -0,0 +1,134 @@
+$(function(event)
+{
+  var txtUri = $('#txtUri');
+  var btnPlayStop = $('#btnPlayStop');
+  var video = $('#video');
+  var rangeVolume = $('#rangeVolume');
+
+  console = new Console('console', console);
+
+  console.iframe = function(url)
+  {
+    var iframe = document.createElement('IFRAME')
+        iframe.src = url;
+
+    this._append(iframe);
+  }
+
+
+  var conn = null;
+
+  function destroyConnection()
+  {
+    // Enable connect button
+    btnPlayStop.html("Play");
+    btnPlayStop.attr('disabled', false);
+
+    txtUri.attr('disabled', false);
+
+    conn = null;
+  }
+
+  function initConnection()
+  {
+    // Set connection success and error events
+    conn.onstart = function(event)
+    {
+      console.log("Connection started");
+
+      // Enable terminate button
+      btnPlayStop.html("Stop");
+      btnPlayStop.attr('disabled', false);
+    };
+    conn.onterminate = function(event)
+    {
+      console.log("Connection terminated");
+    };
+
+    conn.onremotestream = function(event)
+    {
+      console.info("RemoteStream set to "+event.data);
+    }
+
+    conn.onmediaevent = function(event)
+    {
+      var data = event.data;
+
+/*      switch(data.type)
+      {
+        case "url":
+          console.iframe(data.data);
+          break;
+
+        default:
+          console.info(data.type+" => "+data.data);
+      }
+*/
+
+      data = data.data;
+      if(data.substr(0, 4) == "http")
+        console.iframe(data);
+      else
+        console.info(data);
+    }
+
+    conn.onerror = function(error)
+    {
+      destroyConnection()
+
+      // Notify to the user of the error
+      console.error(error.message);
+    };
+  }
+
+
+  btnPlayStop.on('click', function(event)
+  {
+    // Disable terminate button
+    btnPlayStop.attr('disabled', true);
+
+    if(conn)
+    {
+      // Terminate the connection
+      conn.terminate();
+
+      console.log("Connection terminated by user");
+
+      destroyConnection()
+    }
+
+    else
+    {
+      txtUri.attr('disabled', true);
+
+      // Create a new connection
+      var uri = txtUri.val();
+
+      var options =
+      {
+        remoteVideoTag: 'video'
+      };
+
+      try
+      {
+        conn = new KwsContent(uri, options);
+
+        console.log("Connection created pointing to '"+uri+"'");
+
+        initConnection();
+      }
+      catch(error)
+      {
+        destroyConnection()
+
+        // Notify to the user of the error
+        console.error(error.message)
+      }
+    }
+  });
+
+  rangeVolume.on('change', function(event)
+  {
+    video[0].volume = rangeVolume.val();
+  })
+});
