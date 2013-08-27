eabbdd9ede3cfd244ccd890b5e045c507e70d495
Luis López <lulop@kurento.com>
2013-08-27 11:28:08 +0200

    Modified look and feel preparing for London's Campus Party
    Change-Id: Ib88dba559f1422dfdf5e1d91c003e01dd4b2ceec

--- src/main/resources/demo/js/index.js
+++ src/main/resources/demo/js/index.js
@@ -1,116 +1,110 @@
-$(function(event)
-{
-  var txtUri = $('#txtUri');
-  var btnPlayStop = $('#btnPlayStop');
-  var video = $('#video');
-  var rangeVolume = $('#rangeVolume');
-  var viewer = $('#viewer');
 
-  console = new Console('console', console);
+$(function(event) {
+	var txtUri = $('#txtUri');
+	var btnPlayStop = $('#btnPlayStop');
+	var video = $('#video');
+	var rangeVolume = $('#rangeVolume');
+	var viewer = $('#viewer');
+	var eventTypeTxt = $('#eventTypeTxt');
+	var eventValueTxt = $('#eventValueTxt');
 
+	console = new Console('console', console);
 
-  var conn = null;
+	var conn = null;
 
-  function destroyConnection()
-  {
-    // Enable connect button
-    btnPlayStop.html("Play");
-    btnPlayStop.attr('disabled', false);
+	function destroyConnection() {
+		// Enable connect button
+		btnPlayStop.html("Play");
+		btnPlayStop.attr('disabled', false);
 
-    txtUri.attr('disabled', false);
+		txtUri.attr('disabled', false);
 
-    conn = null;
-  }
+		conn = null;
+	}
 
-  function initConnection()
-  {
-    // Set connection success and error events
-    conn.onstart = function(event)
-    {
-      console.log("Connection started");
+	function initConnection() {
+		// Set connection success and error events
+		conn.onstart = function(event) {
+			console.log("Connection started");
 
-      // Enable terminate button
-      btnPlayStop.html("Stop");
-      btnPlayStop.attr('disabled', false);
-    };
-    conn.onterminate = function(event)
-    {
-      console.log("Connection terminated");
-    };
+			// Enable terminate button
+			btnPlayStop.html("Stop");
+			btnPlayStop.attr('disabled', false);
+		};
+		conn.onterminate = function(event) {
+			console.log("Connection terminated");
+		};
 
-    conn.onremotestream = function(event)
-    {
-      console.info("RemoteStream set to "+event.data);
-    }
+		conn.onremotestream = function(event) {
+			console.info("RemoteStream set to " + event.data);
+		}
 
-    conn.onmediaevent = function(event)
-    {
-      var data = event.data;
+		conn.onmediaevent = function(event) {
+			var data = event.data;
 
-      data = data.data;
-      if(data.substr(0, 4) == "http")
-        viewer.attr('src', data);
-      else
-        console.info(data);
-    }
+			eventTypeTxt.text("Event type: " + data.type);
+			eventValueTxt.text("Event data: " + data.data);
 
-    conn.onerror = function(error)
-    {
-      destroyConnection()
+			data = data.data;
+			if (data.substr(0, 4) == "http") {
+				// Make event arrow to glitch
+				$('#arrowPanel').fadeIn('slow', function(event) {
+					$('#arrowPanel').fadeOut('slow')
+				});
+				// Make appear iframe content
+				viewer.attr('src', data);
+			} else {
+				console.info(data);
+			}
+		}
 
-      // Notify to the user of the error
-      console.error(error.message);
-    };
-  }
+		conn.onerror = function(error) {
+			destroyConnection()
 
+			// Notify to the user of the error
+			console.error(error.message);
+		};
+	}
 
-  btnPlayStop.on('click', function(event)
-  {
-    // Disable terminate button
-    btnPlayStop.attr('disabled', true);
+	btnPlayStop.on('click', function(event) {
+		// Disable terminate button
+		btnPlayStop.attr('disabled', true);
 
-    if(conn)
-    {
-      // Terminate the connection
-      conn.terminate();
+		if (conn) {
+			// Terminate the connection
+			conn.terminate();
 
-      console.log("Connection terminated by user");
+			console.log("Connection terminated by user");
 
-      destroyConnection()
-    }
+			destroyConnection()
+		}
 
-    else
-    {
-      txtUri.attr('disabled', true);
+		else {
+			txtUri.attr('disabled', true);
 
-      // Create a new connection
-      var uri = txtUri.val();
+			// Create a new connection
+			var uri = txtUri.val();
 
-      var options =
-      {
-        remoteVideoTag: 'video'
-      };
+			var options = {
+				remoteVideoTag : 'video'
+			};
 
-      try
-      {
-        conn = new KwsContent(uri, options);
+			try {
+				conn = new KwsContent(uri, options);
 
-        console.log("Connection created pointing to '"+uri+"'");
+				console.log("Connection created pointing to '" + uri + "'");
 
-        initConnection();
-      }
-      catch(error)
-      {
-        destroyConnection()
+				initConnection();
+			} catch (error) {
+				destroyConnection()
 
-        // Notify to the user of the error
-        console.error(error.message)
-      }
-    }
-  });
+				// Notify to the user of the error
+				console.error(error.message)
+			}
+		}
+	});
 
-  rangeVolume.on('change', function(event)
-  {
-    video[0].volume = rangeVolume.val();
-  })
+	rangeVolume.on('change', function(event) {
+		video[0].volume = rangeVolume.val();
+	})
 });
