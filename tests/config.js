7e9ab52f9b8190e5985882ba50e3646e19ab5eb5
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2013-09-25 16:34:20 +0200

    Fixed tests so they can be used easily by Selenium in other contexts
    Change-Id: I1cd933d07207dd9c57d4d69a14da76b1da014d2f

---
+++ tests/config.js
@@ -0,0 +1,10 @@
+var config =
+{
+  videoUrls:
+  {
+    test1: "http://193.147.51.29:8080/content-api-test/playerJson",
+    test2: "http://193.147.51.29:8080/content-api-test/playerJson",
+    test3: "http://193.147.51.29:8080/content-api-test/playerJsonJack",
+    test4: "http://193.147.51.29:8080/content-api-test/playerJsonZBar"
+  }
+};
